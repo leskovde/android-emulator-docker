8cbc58721e59c2fa29f17a7304e4c7757d800045
leskovde <leskovar.denis@seznam.cz>
2024-02-11 22:18:45 +0100

    Fix wording

--- start_emu.sh
+++ start_emu.sh
@@ -9,10 +9,10 @@ NC='\033[0m' # No Color
 function wait_emulator_to_be_ready() {
   emulator_name=${EMULATOR_NAME}
 
-  # ARM instances:
+  # bare-metal instances:
   emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -skip-adb-auth -no-boot-anim -show-kernel
 
-  # x86 instances:
+  # emulated instances:
   #emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -skip-adb-auth -no-boot-anim -show-kernel -qemu -cpu max -machine gic-version=max
 
   printf "${G}==>  ${BL}Emulator has ${YE}${EMULATOR_NAME} ${BL}started in headed mode! ${G}<==${NC}""\n"
