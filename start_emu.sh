232a031049d3ba9a63f5cca58c18144cd12ac887
leskovde <leskovar.denis@seznam.cz>
2024-02-18 14:02:19 +0100

    Add accel check to headed emulator start

--- start_emu.sh
+++ start_emu.sh
@@ -6,14 +6,38 @@ RED='\033[0;31m'
 YE='\033[1;33m'
 NC='\033[0m' # No Color
 
+function check_hardware_acceleration() {
+    if [[ "$HW_ACCEL_OVERRIDE" != "" ]]; then
+        hw_accel_flag="$HW_ACCEL_OVERRIDE"
+    else
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            # macOS-specific hardware acceleration check
+            HW_ACCEL_SUPPORT=$(sysctl -a | grep -E -c '(vmx|svm)')
+        else
+            # generic Linux hardware acceleration check
+            HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
+        fi
+
+        if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
+            hw_accel_flag="-accel off"
+        else
+            hw_accel_flag="-accel on"
+        fi
+    fi
+
+    echo "$hw_accel_flag"
+}
+
+hw_accel_flag=$(check_hardware_acceleration)
+
 function wait_emulator_to_be_ready() {
   emulator_name=${EMULATOR_NAME}
 
   # bare-metal instances:
-  emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -skip-adb-auth -no-boot-anim -show-kernel
+  emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -no-boot-anim -show-kernel ${hw_accel_flag}
 
   # emulated instances:
-  #emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -skip-adb-auth -no-boot-anim -show-kernel -qemu -cpu max -machine gic-version=max
+  #emulator -avd "${emulator_name}" -no-audio -ports 5554,5555 -no-boot-anim -show-kernel ${hw_accel_flag} -qemu -cpu max -machine gic-version=max
 
   printf "${G}==>  ${BL}Emulator has ${YE}${EMULATOR_NAME} ${BL}started in headed mode! ${G}<==${NC}""\n"
 }
